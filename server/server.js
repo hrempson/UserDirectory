5410801dd0abdfd387b0c89d31ee11681efd81a4
Heather Empson <hrempson@gmail.com>
2020-08-19 21:45:38 -0700

    server is listening

--- server/server.js
+++ server/server.js
@@ -1,13 +1,11 @@
-const express = require ("express");
 const path = require ("path");
-const app = express;
-const port = process.env.PORT || 3000
-
+const express = require ("express");
+const app = express();
 const publicPath = path.join(__dirname, "..", "public");
+const port = process.env.PORT || 3000;
 
-app.use (express.static(publicPath));
-
-app.length("*", (req, res) => {
+app.use(express.static(publicPath));
+app.get("*", (req, res) => {
     res.sendFile(path.join(publicPath, "index.html"));
 });
 
